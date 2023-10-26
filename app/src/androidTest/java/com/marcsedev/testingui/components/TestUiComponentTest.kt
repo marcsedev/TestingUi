68f98eb2d7746dd93aa240bb10e83d2fa4476e41
Marc Sogas Esteve <marc.sogas@gmail.com>
2023-10-26 13:53:08 +0200

    Add first test ui Finder

--- app/src/androidTest/java/com/marcsedev/testingui/components/TestUiComponentTest.kt
+++ app/src/androidTest/java/com/marcsedev/testingui/components/TestUiComponentTest.kt
@@ -1,10 +1,33 @@
 package com.marcsedev.testingui.components
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithContentDescription
+import androidx.compose.ui.test.onAllNodesWithTag
+import androidx.compose.ui.test.onAllNodesWithText
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import org.junit.Rule
+import org.junit.Test
 
 class TestUiComponentTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
+
+    @Test
+    fun myFirstTest() {
+        composeTestRule.setContent {
+            TestUiComponent()
+        }
+
+        //FINDER
+        composeTestRule.onNodeWithText("ada lovelace", ignoreCase = true)
+        composeTestRule.onNodeWithTag("component2")
+        composeTestRule.onNodeWithContentDescription("profileImage")
+
+        composeTestRule.onAllNodesWithText("a", ignoreCase = true)
+        composeTestRule.onAllNodesWithTag("component2")
+        composeTestRule.onAllNodesWithContentDescription("profileImage")
+    }
 }
--- app/src/main/java/com/marcsedev/testingui/components/TestUiComponent.kt
+++ app/src/main/java/com/marcsedev/testingui/components/TestUiComponent.kt
@@ -1,7 +1,22 @@
 package com.marcsedev.testingui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 
 @Composable
 fun TestUiComponent() {
+    Column(Modifier.fillMaxSize()) {
+        Text("Ada Lovelace", Modifier.testTag("component1"))
+        Text("Hedy Lamarr", Modifier.testTag("component2"))
+
+        Image(Icons.Rounded.AccountCircle, contentDescription = "profileImage")
+
+    }
 }
