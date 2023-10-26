292a965232145e4441ec9530c363328ca0910e2a
Marc Sogas Esteve <marc.sogas@gmail.com>
2023-10-26 14:07:16 +0200

    Add Actions

--- app/src/androidTest/java/com/marcsedev/testingui/components/TestUiComponentTest.kt
+++ app/src/androidTest/java/com/marcsedev/testingui/components/TestUiComponentTest.kt
@@ -1,12 +1,26 @@
 package com.marcsedev.testingui.components
 
+import androidx.compose.ui.test.doubleClick
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.longClick
 import androidx.compose.ui.test.onAllNodesWithContentDescription
 import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onAllNodesWithText
+import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performImeAction
+import androidx.compose.ui.test.performScrollTo
+import androidx.compose.ui.test.performTextClearance
+import androidx.compose.ui.test.performTextInput
+import androidx.compose.ui.test.performTextReplacement
+import androidx.compose.ui.test.performTouchInput
+import androidx.compose.ui.test.swipeDown
+import androidx.compose.ui.test.swipeLeft
+import androidx.compose.ui.test.swipeRight
+import androidx.compose.ui.test.swipeUp
 import org.junit.Rule
 import org.junit.Test
 
@@ -29,5 +43,28 @@ class TestUiComponentTest {
         composeTestRule.onAllNodesWithText("a", ignoreCase = true)
         composeTestRule.onAllNodesWithTag("component2")
         composeTestRule.onAllNodesWithContentDescription("profileImage")
+
+        //ACTIONS
+        composeTestRule.onNodeWithText("ada lovelace", ignoreCase = true).performClick()
+        composeTestRule.onAllNodesWithText("a").onFirst().performClick()
+        composeTestRule.onNodeWithText("ada lovelace").performTouchInput {
+                longClick()
+                doubleClick()
+                swipeDown()
+                swipeUp()
+                swipeLeft()
+                swipeRight()
+            }
+        composeTestRule.onNodeWithText("ada lovelace").performScrollTo() //scrolling
+        composeTestRule.onNodeWithText("ada lovelace").performImeAction() //keyboard button action        composeTestRule.onNodeWithText("ada lovelace", ignoreCase = true)
+        composeTestRule.onNodeWithText("ada lovelace").performTextClearance() //Delete TextField
+        composeTestRule.onNodeWithText("ada lovelace").performTextInput("add text") //Add to component
+        composeTestRule.onNodeWithText("ada lovelace").performTextReplacement("replace text") //Replace text
+        composeTestRule.onNodeWithText("ada lovelace")
+            .performScrollTo()
+            .performClick()
+            .performImeAction() //add multiactions
+
+
     }
 }
--- app/src/main/java/com/marcsedev/testingui/components/TestUiComponent.kt
+++ app/src/main/java/com/marcsedev/testingui/components/TestUiComponent.kt
@@ -5,14 +5,27 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TestUiComponent() {
+    var name by rememberSaveable {
+        mutableStateOf("TEST")
+    }
+
     Column(Modifier.fillMaxSize()) {
+        TextField(value = name, onValueChange = {name = it})
+        Text(text = "Your name is $name")
         Text("Ada Lovelace", Modifier.testTag("component1"))
         Text("Hedy Lamarr", Modifier.testTag("component2"))
 
